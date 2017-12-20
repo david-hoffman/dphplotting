ad0fd80d1b69343cd922d6ae9bfe1a4e4570b7d5
David Hoffman <dave.p.hoffman@gmail.com>
2017-12-20 17:25:06 -0500

    add colorline

--- __init__.py
+++ __init__.py
@@ -9,6 +9,8 @@ import numpy as np
 import warnings
 from functools import partial
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm, Colormap
 # fancy subplot layout
 import matplotlib.gridspec as gridspec
 from dphutils import fft_gaussian_filter
@@ -19,6 +21,69 @@ try:
     pyfftw.interfaces.cache.enable()
 except ImportError:
     from numpy.fft import rfftn, rfftfreq
+
+
+# Topics: line, color, LineCollection, cmap, colorline, codex
+'''
+Defines a function colorline that draws a (multi-)colored 2D line with coordinates x and y.
+The color is taken from optional data in z, and creates a LineCollection.
+
+z can be:
+- empty, in which case a default coloring will be used based on the position along the input arrays
+- a single number, for a uniform color [this can also be accomplished with the usual plt.plot]
+- an array of the length of at least the same length as x, to color according to this data
+- an array of a smaller length, in which case the colors are repeated along the curve
+
+The function colorline returns the LineCollection created, which can be modified afterwards.
+
+See also: plt.streamplot
+'''
+
+# Data manipulation:
+
+def make_segments(x, y):
+    '''
+    Create list of line segments from x and y coordinates, in the correct format for LineCollection:
+    an array of the form   numlines x (points per line) x 2 (x and y) array
+    '''
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    
+    return segments
+
+
+# Interface to LineCollection:
+
+def colorline(x, y, z=None, cmap='inferno', norm=plt.Normalize(0.0, 1.0), linewidth=3, alpha=1.0, ax=None):
+    '''
+    Plot a colored line with coordinates x and y
+    Optionally specify colors in the array z
+    Optionally specify a colormap, a norm function and a line width
+    '''
+    
+    if not isinstance(cmap, Colormap):
+        cmap = plt.get_cmap(cmap)
+
+    # Default colors equally spaced on [0,1]:
+    if z is None:
+        z = np.linspace(0.0, 1.0, len(x))
+           
+    # Special case if a single number:
+    if not hasattr(z, "__iter__"):  # to check for numerical input -- this is a hack
+        z = np.array([z])
+        
+    z = np.asarray(z)
+    
+    segments = make_segments(x, y)
+    lc = LineCollection(segments, array=z, cmap=cmap, norm=norm, linewidth=linewidth, alpha=alpha)
+    
+    if ax is None:
+        ax = plt.gca()
+    ax.add_collection(lc)
+    ax.autoscale()
+    
+    return lc
 
 
 def display_grid(data, showcontour=False, contourcolor='w', filter_size=None,
